cc28ff08bfa84ce2b39d7bc964b7d0ee051767f9
Margaret <eutopia@gmail.com>
2016-11-09 12:59:45 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+// Create a new component. This component should produce some HTML
+// Take this component's generated HTML and put it on the page (in the DOM).
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
